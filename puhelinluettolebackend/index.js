c09d5c0b71d685c081c4180132a84454a4cbbd68
MJL7068 <mattil@netti.fi>
2019-01-26 00:27:12 +0200

    3.7 done

--- puhelinluettolebackend/index.js
+++ puhelinluettolebackend/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
-const app = express()
+var morgan = require('morgan')
 const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+
+morgan.token('data', (req) => {
+    return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 app.use(bodyParser.json())
 
