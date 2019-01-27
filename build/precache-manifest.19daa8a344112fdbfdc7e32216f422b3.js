9ea53c45ba793e80c7a500b9051c977f3c089f6d
MJL7068 <mattil@netti.fi>
2019-01-27 23:21:08 +0200

    build added

---
+++ build/precache-manifest.19daa8a344112fdbfdc7e32216f422b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12a2da51785c1397c732",
+    "url": "/static/js/main.12a2da51.chunk.js"
+  },
+  {
+    "revision": "1bdeb50c48bfe976fdd4",
+    "url": "/static/js/1.1bdeb50c.chunk.js"
+  },
+  {
+    "revision": "12a2da51785c1397c732",
+    "url": "/static/css/main.87e28bb6.chunk.css"
+  },
+  {
+    "revision": "ff8bb31dd83b340bba0b8476aba46199",
+    "url": "/index.html"
+  }
+];
