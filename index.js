a7aebdf947d224c1c5b85ef57d3c89db0d7af8cf
MJL7068 <mattil@netti.fi>
2019-01-28 23:26:52 +0200

    static added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 const app = express()
 app.use(bodyParser.json())
+app.use(express.static('build'))
 
 morgan.token('data', (req) => {
     return JSON.stringify(req.body)
