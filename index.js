e4501bcf93e374be4d31660981aa317292400754
MJL7068 <mattil@netti.fi>
2019-02-02 00:46:54 +0200

    3.15 done

--- index.js
+++ index.js
@@ -37,12 +37,6 @@ app.get('/api/persons/:id', (req, res) => {
     })
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
-    res.status(204).end()
-})
-
 app.post('/api/persons', (req, res) => {
     const body = req.body
 
@@ -58,19 +52,6 @@ app.post('/api/persons', (req, res) => {
         })
     }
 
-    /*if (persons.find(p => p.name === body.name)) {
-        return res.status(400).json({
-            error: 'name must be unique'
-        })
-    }*/
-
-    /*const person = {
-        id: generateId(),
-        name: body.name,
-        number: body.number,
-    }*/
-
-    //persons = persons.concat(person)
     const person = new Person({
         name: body.name,
         number: body.number,
@@ -81,6 +62,16 @@ app.post('/api/persons', (req, res) => {
       })
 
     res.json(person)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    console.log("hello")
+    console.log(req.params.id)
+    Person.findByIdAndRemove(req.params.id)
+      .then(result => {
+          response.status(204).end
+      })
+      .catch(error => console.log(error.message))
 })
 
 const PORT = process.env.PORT || 3001
